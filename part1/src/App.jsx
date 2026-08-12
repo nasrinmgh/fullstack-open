e6175a472d1ed361e69a39696c74e15dcd9e0675
nasrinmgh <nasrinmohaghegh98@gmail.com>
2026-08-12 10:02:18 +0200

    part1 - exercise 1.11

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,9 +6,10 @@ const Button = ({ onClick, text }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>
-      {text} : {value}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
@@ -18,12 +19,16 @@ const Statistics = ({ good, neutral, bad, total, average, positive }) => {
   }
   return (
     <>
-      <StatisticLine text={"Good"} value={good} />
-      <StatisticLine text={"Neutral"} value={neutral} />
-      <StatisticLine text={"Bad"} value={bad} />
-      <StatisticLine text={"All"} value={total} />
-      <StatisticLine text={"Average"} value={average} />
-      <StatisticLine text={"Positive"} value={`${positive} %`} />
+      <table>
+        <tbody>
+          <StatisticLine text={"Good"} value={good} />
+          <StatisticLine text={"Neutral"} value={neutral} />
+          <StatisticLine text={"Bad"} value={bad} />
+          <StatisticLine text={"All"} value={total} />
+          <StatisticLine text={"Average"} value={average} />
+          <StatisticLine text={"Positive"} value={`${positive} %`} />
+        </tbody>
+      </table>
     </>
   );
 };
