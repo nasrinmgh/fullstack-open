701531e452cdb243e66819c60d95df474a018a05
nasrinmgh <nasrinmohaghegh98@gmail.com>
2026-08-19 19:31:27 +0200

    part2b - 2.6

--- part2/phonebook/phonebook/src/App.jsx
+++ part2/phonebook/phonebook/src/App.jsx
@@ -1,22 +1,41 @@
 import { useState } from "react";
+import Person from "./components/Person";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: "Arto" }]);
   const [newName, setNewName] = useState("");
+
+  const handleAddPerson = (event) => {
+    event.preventDefault();
+    const person = {
+      name: newName,
+      id: crypto.randomUUID(),
+    };
+    setPersons(persons.concat(person));
+    setNewName("");
+  };
+
+  const handleNewPerson = (event) => {
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleAddPerson}>
         <div>
-          name: <input />
+          name: <input onChange={handleNewPerson} value={newName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map((person) => (
+          <Person key={person.id} person={person} />
+        ))}
+      </div>
     </div>
   );
 };
